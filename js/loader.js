d98228facde93cbab162d4f34df35657813aec8a
Dave Vineis <dave.vineis@gmail.com>
2022-09-16 10:11:00 -0500

    new video

---
+++ js/loader.js
@@ -0,0 +1,14 @@
+/* page loading */
+var duration = 5;
+var loaded = null;
+var loadListener = function() {
+	document.body.className += " loaded";
+}
+
+window.setTimeout(function(){
+	if(loaded == true)loadListener();
+}, duration*1000);
+window.addEventListener("load", function(){
+	if(loaded == null)loaded = true;
+	else loadListener();
+});
